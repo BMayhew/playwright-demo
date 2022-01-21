711067e80b2e22cc6466cfd02fac0c624f6e7dd7
Butch Mayhew <bmayhew@tilled.com>
2022-01-20 18:42:21 -0600

    adding ppt

--- tests/ui/login-user.spec.ts
+++ tests/ui/login-user.spec.ts
@@ -17,7 +17,7 @@ test("Successfully Login", async ({ page, baseURL }) => {
 
   await page.locator("[data-qa=login-email]").fill(username);
   await page.locator("[data-qa=login-password]").fill(password);
-  await page.locator("text=Login").click(); //this is currently broken on purpose
+  await page.locator("button:has-text('Login')").click(); //this is currently was broken on purpose
   expect(await page.locator("header")).toContainText("Logged in as Testy");
 });
 
