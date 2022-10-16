b3956dc32dd0f7d703e62ba0370f3ef89238ef73
Butch Mayhew <bmayhew@tilled.com>
2022-10-15 23:27:28 -0500

    ading accessibility test it will fail (#8)
    * ading accessibility test it will fail
    
    * updating actions file
    
    * updating to latest deploy
    
    * updating to add content write
    
    * updating action
    
    * updating

---
+++ tests/ui/accessibility.spec.ts
@@ -0,0 +1,24 @@
+import { test, expect } from "@playwright/test";
+import AxeBuilder from "@axe-core/playwright";
+
+test("Visit home page and run an axe test @axe", async ({ page }, testInfo) => {
+  await page.goto("https://broken-workshop.dequelabs.com/");
+
+  //Analyze the page with axe
+  const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+
+  //Attached the violations to the test report
+  await testInfo.attach("accessibility-scan-results", {
+    body: JSON.stringify(accessibilityScanResults.violations, null, 2),
+    contentType: "application/json",
+  });
+
+  //Console log the violations
+  let violation = accessibilityScanResults.violations;
+  violation.forEach(function (entry) {
+    console.log(entry.impact + " " + entry.description);
+  });
+
+  //Expect violations to be empty
+  expect(accessibilityScanResults.violations).toEqual([]);
+});
