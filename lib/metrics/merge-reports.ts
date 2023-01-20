663ea89ea996878b270c10726099e167f380ab37
Butch Mayhew <bmayhew@tilled.com>
2023-01-19 18:34:57 -0600

    addng ts-node

--- lib/metrics/merge-reports.ts
+++ lib/metrics/merge-reports.ts
@@ -8,7 +8,7 @@ mergeHTMLReports(
     path.resolve(process.cwd(), "./playwright-report-2_5"),
     path.resolve(process.cwd(), "./playwright-report-3_5"),
     path.resolve(process.cwd(), "./playwright-report-4_5"),
-    path.resolve(process.cwd(), "./playwright-report-_5"),
+    path.resolve(process.cwd(), "./playwright-report-5_5"),
   ],
   { outputFolderName: "./test-merge-report-demo" }
 );
