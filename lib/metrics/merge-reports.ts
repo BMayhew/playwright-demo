211bdf4b386aadd7a3a58fd934a8822593b59f34
Butch Mayhew <bmayhew@tilled.com>
2023-01-19 22:24:46 -0600

    testing

--- lib/metrics/merge-reports.ts
+++ lib/metrics/merge-reports.ts
@@ -1,19 +1,30 @@
+import fs from "fs";
 import path from "path";
 const { mergeHTMLReports } = require("playwright-merge-html-reports");
-// import { mergeHTMLReports } from "playwright-merge-html-reports";
 
-mergeHTMLReports(
-  [
-    path.resolve(process.cwd(), "./playwright-report-1_5"),
-    path.resolve(process.cwd(), "./playwright-report-2_5"),
-    path.resolve(process.cwd(), "./playwright-report-3_5"),
-    path.resolve(process.cwd(), "./playwright-report-4_5"),
-    path.resolve(process.cwd(), "./playwright-report-5_5"),
-  ],
-  { outputFolderName: "./test-merge-report-demo" }
-);
+// mergeHTMLReports(
+//   [
+//     path.resolve(process.cwd(), "./playwright-report-1_5"),
+//     path.resolve(process.cwd(), "./playwright-report-2_5"),
+//     path.resolve(process.cwd(), "./playwright-report-3_5"),
+//     path.resolve(process.cwd(), "./playwright-report-4_5"),
+//     path.resolve(process.cwd(), "./playwright-report-5_5"),
+//   ],
+//   { outputFolderName: "./test-merge-report-demo" }
+// );
 
-// test("Merge Reports", async () => {
-//   mergeHTMLReports;
-//   expect(1).toBe(1);
-// });
+// console.log(
+//   fs
+//     .readdirSync(process.cwd() + "/playwright-report", { withFileTypes: true })
+//     .filter((item) => item.isDirectory())
+//     .map(({ name }) => path.resolve(process.cwd() + "/playwright-report", name))
+// );
+
+const reportPathsToMerge = fs
+  .readdirSync(process.cwd() + "/playwright-report", { withFileTypes: true })
+  .filter((item) => item.isDirectory())
+  .map(({ name }) => path.resolve(process.cwd() + "/playwright-report", name));
+
+mergeHTMLReports(reportPathsToMerge, {
+  outputFolderName: "html-report",
+});
