42e8f9287923bb6563a6c78e4838fb601d00a4a2
Butch Mayhew <bmayhew@tilled.com>
2023-02-26 23:24:16 -0600

    adding toHaveAttribute example

---
+++ tests/ui/codepen/buttonToHaveAttribute.spec.ts
@@ -0,0 +1,23 @@
+import { test, expect } from "@playwright/test";
+
+test("test", async ({ page }) => {
+  await page.goto("https://codepen.io/bmayhew/pen/eYLdwVg");
+
+  // Create variables for the elements we will interact with
+  const codePenFrame = page.frameLocator('iframe[name="CodePen"]');
+  const textInput = codePenFrame.getByPlaceholder("fill me");
+  const button = codePenFrame.getByRole("button", { name: "Click Me" });
+  const result = codePenFrame.locator("id=result");
+
+  // Disabled attribute is active
+  expect(button).toHaveAttribute("disabled", "true");
+
+  await textInput.fill("Testing 1234");
+  await page.keyboard.press("Tab");
+
+  // Disabled attribute is no longer active
+  expect(button).not.toHaveAttribute("disabled", "true");
+
+  await button.click();
+  expect(result).toHaveText("You clicked the button");
+});
