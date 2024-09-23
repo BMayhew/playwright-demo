b4a70c987d638bb278a3cf13ce36bb257d130cd4
Butch Mayhew <bmayhew@tilled.com>
2024-09-23 07:42:35 -0500

    adding npm script and updating a helper file

--- lib/network/abortImages.ts
+++ lib/network/abortImages.ts
@@ -4,7 +4,7 @@ export const test = base.extend({
   page: async ({ page }, use) => {
     await page.route("**/*.{png,jpg,jpeg}", (route) => route.abort());
     await page.route("**/get_product_picture/**", (route) => route.abort());
-
+    // this is a comment
     await use(page);
   },
 });
